e4176a00b55128e0238cf636bd9000f46cf6ab4a
Pavel Naumenko <p.naumenko@evotor.ru>
2020-04-13 21:00:40 +0300

    Добавил тестплан, и тестовый класс

---
+++ SwiftyGiphyUITests/TestPlans/123.swift
@@ -0,0 +1,9 @@
+//
+//  123.swift
+//  SwiftyGiphyUITests
+//
+//  Created by Pavel Naumenko on 13.04.2020.
+//  Copyright © 2020 andrewlupul. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftyGiphyUITests/Tests/BaseTestCase.swift
@@ -0,0 +1,18 @@
+//
+//  BaseTestCase.swift
+//  SwiftyGiphyUITests
+//
+//  Created by Pavel Naumenko on 13.04.2020.
+//  Copyright © 2020 andrewlupul. All rights reserved.
+//
+
+import XCTest
+
+class BaseTestCase: XCTestCase {
+
+    func testSomeTest() {
+        XCUIApplication().launch()
+
+    }
+
+}
